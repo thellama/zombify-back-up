a9764c548e6ba4792cf8dcc82ec7d67553bf229b
Matt McCandless <mmccandless11@gmail.com>
2013-09-21 17:00:27 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zombify::Application.config.secret_key_base = '579ad155c081b0e7f1f19cbf671e0132bfe0f86645b6a973f9ac98aa244a666c4fdd4da49ac11da521c12863c590492608f7a0baa134719338297f445cc714d7'
