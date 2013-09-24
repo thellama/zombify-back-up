5957ae10db19ec43ef80665a4112437ab8ca92cd
Matt McCandless <mmccandless11@gmail.com>
2013-09-24 15:56:53 -0400

    Change Default Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Zombify::Application.routes.draw do
 
   resources :users
 
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
